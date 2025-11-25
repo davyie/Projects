d4b1f1b145df1e2b767d3dcab863d97c960246ea
David Yu <yu.davidcheng@gmail.com>
2025-11-25 08:38:17 +0000

    Add Caching proxy project

---
+++ caching_proxy/src/main/java/com/roadmap/cachine_proxy/CachineProxyApplication.java
@@ -0,0 +1,13 @@
+package com.roadmap.cachine_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachineProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachineProxyApplication.class, args);
+	}
+
+}
---
+++ caching_proxy/src/test/java/com/roadmap/cachine_proxy/CachineProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmap.cachine_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachineProxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
