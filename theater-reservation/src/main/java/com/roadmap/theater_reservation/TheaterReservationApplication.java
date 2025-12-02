1fd61897413e98a76bcf4f5430788a0f88c1a9ad
David Yu <yu.davidcheng@gmail.com>
2025-12-02 15:36:52 +0000

    Add new project

---
+++ theater-reservation/src/main/java/com/roadmap/theater_reservation/TheaterReservationApplication.java
@@ -0,0 +1,13 @@
+package com.roadmap.theater_reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterReservationApplication.class, args);
+	}
+
+}
