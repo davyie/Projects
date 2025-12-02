c04d51f7391c7378b96b35134f38c238565cad13
David Yu <yu.davidcheng@gmail.com>
2025-12-02 16:18:33 +0000

    Add hello world controller

--- theater-reservation/src/main/java/com/roadmap/theater_reservation/TheaterReservationApplication.java
+++ theater-reservation/src/main/java/com/roadmap/theater_reservation/TheaterReservationApplication.java
@@ -2,6 +2,7 @@ package com.roadmap.theater_reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 public class TheaterReservationApplication {
