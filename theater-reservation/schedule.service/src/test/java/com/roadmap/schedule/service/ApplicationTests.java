efd97cf86630d175d5f1c04dd77269e4881b3a9b
David Yu <yu.davidcheng@gmail.com>
2026-01-17 18:23:18 +0000

    Add new version of theater reservation system with dedicated maven sub modules to adher to microservice architecture

---
+++ theater-reservation/schedule.service/src/test/java/com/roadmap/schedule/service/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadmap.schedule.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
